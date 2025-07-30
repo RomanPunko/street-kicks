bb0e18fe1db31239e280a656c0703e2ba060df96
Roman Punko <romanpunko2004@gmail.com>
2025-07-30 20:51:48 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <p className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        12321321fdsgfd
+      </p>
+    </div>
+  );
+}
