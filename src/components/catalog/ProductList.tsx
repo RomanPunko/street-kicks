df21c3dc3beb8c3fd4fe1801fae75c5b38f34e83
Roman Punko <romanpunko2004@gmail.com>
2025-09-28 20:37:04 +0300

    fix: priceRange bug

--- src/components/catalog/ProductList.tsx
+++ src/components/catalog/ProductList.tsx
@@ -40,11 +40,7 @@ const ProductList: FC = () => {
     }
 
     if (priceRange) {
-      result = result.filter((item) => {
-        if (item.price >= priceRange[0] && item.price <= priceRange[1]) {
-          return result;
-        }
-      });
+      result = result.filter((item) => item.price >= priceRange[0] && item.price <= priceRange[1]);
     }
 
     return result;
@@ -74,7 +70,13 @@ const ProductList: FC = () => {
   return (
     <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-between gap-4 lg:pl-4 lg:pr-4">
       {filteredProducts.map((item) => (
-        <ProductCard id={item.id} name={item.name} image={item.image} price={item.price} key={item.id} />
+        <ProductCard
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          price={item.price}
+          key={item.id}
+        />
       ))}
     </div>
   );
