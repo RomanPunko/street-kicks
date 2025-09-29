65b80f1631540937067696c2ed01c82427aede27
Roman Punko <romanpunko2004@gmail.com>
2025-09-29 12:56:22 +0300

    fix: page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ProductList from "@/components/catalog/ProductList";
 const Home: FC = () => {
 
   return (
-    <div className="h-full w-full flex pl-2 pr-2 sm:pl-4 sm:pr-4">
+    <div className="h-full w-full flex pl-2 pr-2 sm:pl-4 sm:pr-4 lg:pl-10 lg:pr-10">
       <Sidebar/>
       <ProductList/>
     </div>
